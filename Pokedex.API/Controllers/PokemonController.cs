9177e6ec4261c7db8fec3886daabf5a834a6ffcd
Anton Fomenko <vafon2@gmail.com>
2021-06-25 07:09:46 +0300

    Create pokemon controller.

---
+++ Pokedex.API/Controllers/PokemonController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pokedex.API.Resources;
+using Pokedex.API.Services;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Pokedex.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private readonly IPokemonService _pokemonService;
+
+        public PokemonController(IPokemonService pokemonService)
+        {
+            _pokemonService = pokemonService;
+        }
+
+        [HttpGet]
+        [Route("{name}")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(PokemonResource), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<PokemonResource>> Get(string name)
+        {
+            PokemonResource res = await _pokemonService.Get(name);
+
+            if (res != null)
+                return res;
+
+            return NotFound();
+        }
+
+        [HttpGet]
+        [Route("translated/{name}")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(PokemonResource), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<PokemonResource>> GetTranslated(string name)
+        {
+            PokemonResource res = await _pokemonService.GetTranslatedAsync(name);
+
+            if (res != null)
+                return res;
+
+            return NotFound();
+        }
+    }
+}
---
+++ Pokedex.API/Services/IPokemonService.cs
@@ -0,0 +1,12 @@
+﻿using Pokedex.API.Resources;
+using System.Threading.Tasks;
+
+namespace Pokedex.API.Services
+{
+    public interface IPokemonService
+    {
+        Task<PokemonResource> Get(string name);
+
+        Task<PokemonResource> GetTranslatedAsync(string name);
+    }
+}
