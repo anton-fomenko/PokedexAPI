fb27612149aaf359658d1d7c2e7183ea7c2240b9
Anton Fomenko <vafon2@gmail.com>
2021-06-25 09:51:34 +0300

    Add tests for TextHelper and implement the Fix method.

---
+++ Pokedex.API.UnitTests/TextHelperTests.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Pokedex.API.Helpers;
+
+namespace Pokedex.API.UnitTests
+{
+    [TestFixture]
+    public class TextHelperTests
+    {
+        ITextHelper _textHelper = new TextHelper();
+
+        [Test]
+        public void FixText_NewLine_ReplacedWithSpace()
+        {
+            // Arrange
+            string originalText = "This is\npokemon";
+
+            // Act
+            string fixedText = _textHelper.Fix(originalText);
+
+            // Assert
+            Assert.AreEqual("This is pokemon", fixedText);
+        }
+
+        [Test]
+        public void FixText_PageBreak_ReplacedWithSpace()
+        {
+            // Arrange
+            string originalText = "This is\fpokemon";
+
+            // Act
+            string fixedText = _textHelper.Fix(originalText);
+
+            // Assert
+            Assert.AreEqual("This is pokemon", fixedText);
+        }
+
+        [Test]
+        public void FixText_SoftHyphenFollowedByPageBreak_Removed()
+        {
+            // Arrange
+            string originalText = "con\u00ad\ftinuously";
+
+            // Act
+            string fixedText = _textHelper.Fix(originalText);
+
+            // Assert
+            Assert.AreEqual("continuously", fixedText);
+        }
+
+        [Test]
+        public void FixText_HardHyphens_RemainOnTheirPlace()
+        {
+            // Arrange
+            string originalText = "break-in";
+
+            // Act
+            string fixedText = _textHelper.Fix(originalText);
+
+            // Assert
+            Assert.AreEqual("break-in", fixedText);
+        }
+
+        [Test]
+        public void FixText_HardHyphenFollowedByNewLine_NewLineRemovedButHyphenRemains()
+        {
+            // Arrange
+            string originalText = "break-\nin";
+
+            // Act
+            string fixedText = _textHelper.Fix(originalText);
+
+            // Assert
+            Assert.AreEqual("break-in", fixedText);
+        }
+
+        [Test]
+        public void FixText_DashFollowedByNewLine_JustDashRemains()
+        {
+            // Arrange
+            string originalText = "You may think she is a liar -\nshe isn't.";
+
+            // Act
+            string fixedText = _textHelper.Fix(originalText);
+
+            // Assert
+            Assert.AreEqual("You may think she is a liar - she isn't.", fixedText);
+        }
+    }
+}
--- Pokedex.API/Helpers/TextHelper.cs
+++ Pokedex.API/Helpers/TextHelper.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace Pokedex.API.Helpers
 {
@@ -12,7 +9,19 @@ namespace Pokedex.API.Helpers
             // Original text contains special characters, which were used as a hack to separate text.
             // We need to remove them so that the text could be translated and presented correctly.
 
-            throw new NotImplementedException();
+            // This is a list of the necessary changes:
+
+            // Page breaks are treated just like newlines.
+            // Soft hyphens followed by newlines vanish.
+            // Hard hyphens followed by newlines become just hard hyphens, to preserve real
+            // hyphenation.
+            // Any other newline becomes a space.
+
+            return Regex.Replace(original, "\f", "\n")
+                        .Replace("\u00ad\n", "")
+                        .Replace(" -\n", " - ")
+                        .Replace("-\n", "-")
+                        .Replace("\n", " ");
         }
     }
 }
